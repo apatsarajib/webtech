9e86fbc89636c1590534fa3588693e7c030308b3
apatsarajib <76036835+apatsarajib@users.noreply.github.com>
2020-12-15 16:05:29 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,6 +5,13 @@
     <title>ทดสอบ HTML</title>
   </head>
   <body>
-          สวัสดี ศิลปากร
+          <h1>สวัสดี ศิลปากร</h1>
+          <h2>Web Technology for Data Science</h2>
+          <p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตร์บัณฑิต สาขาวิชาวิทยาการข้อมูล</p>
+          <p>ภาษาอังกฤษ : Bachelor of Science Program in Data Sciemce</p>
+
+
+<a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตร วิทยาการข้อมูล</a>
+
   </body>
 </html>
